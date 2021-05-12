384a69a8f7eb0093a9adfcbeceede24be1b494ee
lucas <laksteelhouse@gmail.com>
2021-05-12 19:11:39 +0200

    Styles contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,14 +4,19 @@ import React from 'react'
 const ContactForm = () => {
   return (
     <div style={styles.container}>
-      <form id='contact-form'>
-        <input type='text' name='user_name' placeholder='Name' />
-        <br />
-        <input type='email' name='user_email' placeholder='Email' />
-        <br />
-        <textarea name='message' placeholder='Message' />
-        <br />
-        <input type='submit' value='Send' />
+      <form id='contact-form' style={{display: 'flex', alignItems: 'center'}}>
+        <div  style={{textAlign: 'center'}}>
+          <input type='text' name='user_name' placeholder='Name' style={styles.input} />
+          <br />
+          <input type='email' name='user_email' placeholder='Email' style={styles.input}/>
+          <br />
+          <input type='submit' value='Send' style={styles.button} />
+        </div>
+        <div>
+          <textarea name='message' placeholder='Message' style={{...styles.input, height: 200}} />
+          <br />
+         
+        </div>
       </form>
     </div>
   )
@@ -26,6 +31,21 @@ const styles = {
     alignItems: 'center',
     height: 300,
     marginBottom: 50,
+  },
+  input: {
+    padding: 10,
+    borderRadius: 5,
+    margin: 25,
+    fontSize: 15
+  },
+  button: {
+    padding: '10px 15px',
+    borderRadius: 10,
+    backgroundColor: '#50df40',
+    color: 'white',
+    border: 'none',
+    fontWeight: 'bold',
+    boxShadow: '0 0 2px 1px black'
   }
 
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,12 +49,13 @@ const Hero = () => {
       <div style={{ padding: '0 20%', height: '50vw', display: 'flex', alignItems: 'center', flex: 1, marginBottom: 100 }}>
         <div style={styles.infoContainer}>
           <p>Try it for free!</p>
-          <h1 style={{ textTransform: 'uppercase'}}>Options for everybody</h1>
+          <h1 style={{ textTransform: 'uppercase' }}>Options for everybody</h1>
           <h3 style={{ color: 'lightgrey', textTransform: 'capitalize' }} >Created to make protection easy</h3>
           <Button style={{ background: `url(${googleplay})`, ...styles.button }} />
           <Button style={{ background: `url(${appstore})`, ...styles.button, }} />
         </div>
         <div style={styles.imgContainer}>
+          <img src={gif} alt='animation' style={{opacity: 0.2, position: 'absolute', transform: 'translate(-8px, 5px)', zIndex: -1, height: '40vw' }} />
           <img src={gif} alt='animation' style={styles.img} />
         </div>
       </div>
